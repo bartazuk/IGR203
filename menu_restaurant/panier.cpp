21e59badc9e04d1233790b7bfba81603a4d4c19e
qiqiqiqi0925 <huyuqi1991@126.com>
2016-04-01 10:52:35 +0200

    Add panier
    ajouter class panier

---
+++ menu_restaurant/panier.cpp
@@ -0,0 +1,54 @@
+#include "panier.h"
+
+panier::~panier()
+{
+}
+
+panier::panier(vector<plat> _listPlat, int _numClient, int _somme, int _numTable){
+
+    listPlat = _listPlat;
+    numClient = _numClient;
+    somme = _somme;
+    numTable = _numTable;
+}
+
+
+vector<plat> panier::getListPlat() const
+{
+    return listPlat;
+}
+
+void panier::setListPlat(const vector<plat> &_listPlat)
+{
+    listPlat = _listPlat;
+}
+
+int panier::getNumClient() const
+{
+    return numClient;
+}
+
+void panier::setNumClient(int _numClient)
+{
+    numClient = _numClient;
+}
+
+int panier::getNumTable() const
+{
+    return numTable;
+}
+
+void panier::setNumTable(int _numTable)
+{
+    numTable = _numTable;
+}
+
+int panier::getSomme() const
+{
+    return somme;
+}
+void panier::setSomme(int _somme)
+{
+    somme=_somme;
+}
+
---
+++ menu_restaurant/panier.h
@@ -0,0 +1,35 @@
+#ifndef PANIER_H
+#define PANIER_H
+
+#include <string>
+#include <vector>
+#include <QString>
+#include "plat.h"
+
+class panier
+{
+public:
+    panier(vector<plat> _listPlat={}, int _numClient=0, int _somme=0, int _numTable=0);
+    ~panier();
+
+    vector<plat> getListPlat() const;
+    void setListPlat(const vector<plat> &value);
+
+    int getNumTable() const;
+    void setNumTable(int value);
+
+    int getNumClient() const;
+    void setNumClient(int value);
+
+    int getSomme() const;
+    void setSomme(int value);
+
+
+private:
+    vector<plat> listPlat;
+    int numClient;
+    int somme;
+    int numTable;
+};
+
+#endif // PANIER_H
